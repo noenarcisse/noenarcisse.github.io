e7c4d8dd6cc576fb127a45b9c2bcefc058f262bb
NoeN <narcisse.noe@gmail.com>
2026-01-24 15:45:25 +0100

    Create About.vue
    prep base module

---
+++ frontend/src/components/About.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+//about module
+
+</script>
+
+<template>
+    <div class="about-container">
+    </div>
+
+</template>
+
+
+<style scoped>
+.container-project 
+{
+
+}
+</style>
