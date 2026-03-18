267939f2c285f26958dd0b687b390d214e5691d1
NoeN <24297098+noenarcisse@users.noreply.github.com>
2026-03-18 16:20:56 +0100

    update project and content

--- frontend/src/components/About.vue
+++ frontend/src/components/About.vue
@@ -54,7 +54,7 @@ const oldTechs:string[] = [
     <div class="about-section">
         <div class="container descr">
             <div class="about">
-                <div class="profile-picture"><img src="/images/profile_pics/aod_mini.jpg" /></div>
+                <div class="profile-picture"><img src="/images/profile_pics/aod_2_mini.jpg" /></div>
                 <div class="name">Noe Narcisse</div>
                 <div class="email"><img src="/email_icon.png" class="small-icons"/><a href="mailto:noenarcisse.job@gmail.com">noenarcisse.job@gmail.com</a></div>
                 <div class="location"><img src="/location_icon.png" class="small-icons"/>Brussels, Belgium</div>
